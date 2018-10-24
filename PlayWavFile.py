c00ed3d35349e3bed262a19fc16a05fb04db28b3
cvaldez83 <cvaldez83@gmail.com>
2018-10-24 12:15:18 -0700

    Update PlayWavFile.py

--- PlayWavFile.py
+++ PlayWavFile.py
@@ -1,2 +1,7 @@
+# 1st method: auto-plays with no window popup
 import winsound
 winsound.PlaySound('filename',winsound.SND_FILENAME)
+
+# 2nd method: Windows media player pops up & auto-plays
+import os
+os.system('filename.wav &') # & is to auto-play
